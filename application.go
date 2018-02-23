fa87e66954ba948dff91995c1f01f469792250e5
celerno <celerno@gmail.com>
2018-02-23 00:29:51 -0600

    preparing for amazon

---
+++ application.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"wwwGo/www"
+	"github.com/kataras/iris"
+	"github.com/kataras/golog"
+	"os"
+)
+func init(){
+	golog.SetLevel("ERROR")
+}
+func main() {
+	app := iris.Default()
+	url := os.Getenv("PORT")
+	if len(url) == 0 {
+		url=":5000"
+	}
+	www.Start(app, url)
+}
---
+++ build.sh
@@ -0,0 +1,5 @@
+go get github.com/kataras/iris
+go get github.com/kataras/golog
+go get github.com/go-floki/jade
+go get github.com/kataras/iris/context
+go get github.com/kataras/iris/websocket
